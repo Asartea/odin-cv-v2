4dbb16e52085c68229938a1a443f71e046bd4f1c
Asartea <76259120+Asartea@users.noreply.github.com>
2025-02-09 11:23:14 +0100

    Add prettier and run formatting (#5)

--- src/components/education/Education.tsx
+++ src/components/education/Education.tsx
@@ -8,16 +8,25 @@ type EducationSectionProps = {
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     addSection: (section: string) => void;
 };
-function EducationSection (props: EducationSectionProps) {
-    const {educations, onChange, addSection} = props;
+function EducationSection(props: EducationSectionProps) {
+    const { educations, onChange, addSection } = props;
 
     return (
         <div>
             <h2>Education Section</h2>
             {Object.entries(educations).map(([key, value]) => (
-                <EducationForm key={key} id={key} data={value} onChange={onChange} />
+                <EducationForm
+                    key={key}
+                    id={key}
+                    data={value}
+                    onChange={onChange}
+                />
             ))}
-            <Button onClick={() => addSection('education')} text='Add Education' className='add-section' />
+            <Button
+                onClick={() => addSection("education")}
+                text="Add Education"
+                className="add-section"
+            />
         </div>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-const rootNode = document.getElementById('root')
+const rootNode = document.getElementById("root");
 createRoot(rootNode).render(
     <StrictMode>
         <App />
     </StrictMode>,
-  )
+);
