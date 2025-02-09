abf8b6aa9e61470b57dc226cde7336cb0a7ac953
bruggenj <bruggenjoris@gmail.com>
2025-02-09 11:22:52 +0100

    Add prettier and run formatting

--- src/components/personal/PersonalPreview.tsx
+++ src/components/personal/PersonalPreview.tsx
@@ -1,19 +1,19 @@
-import {PersonalData} from '../../App'
+import { PersonalData } from "../../App";
 
 type PersonalPreviewProps = {
-    personal: PersonalData
-}
+    personal: PersonalData;
+};
 
-function PersonalPreview({personal}: PersonalPreviewProps) {
+function PersonalPreview({ personal }: PersonalPreviewProps) {
     const { firstName, lastName, email, phone } = personal;
     return (
         <div>
             <h2>Personal Information</h2>
-            <p>Name: {firstName + ' ' + lastName}</p>
+            <p>Name: {firstName + " " + lastName}</p>
             <p>Email: {email}</p>
             <p>Phone: {phone}</p>
         </div>
     );
 }
 
-export default PersonalPreview
+export default PersonalPreview;
--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -7,14 +7,17 @@ type WorkSectionProps = {
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     addSection: (section: string) => void;
 };
-function WorkSection ({experiences, onChange, addSection}: WorkSectionProps) {
+function WorkSection({ experiences, onChange, addSection }: WorkSectionProps) {
     return (
         <div>
             <h2>Work Section</h2>
             {Object.entries(experiences).map(([key, value]) => (
                 <WorkForm key={key} id={key} data={value} onChange={onChange} />
             ))}
-            <Button onClick={() => addSection('work-experience')} text={"Add Work Experience"}></Button>
+            <Button
+                onClick={() => addSection("work-experience")}
+                text={"Add Work Experience"}
+            ></Button>
         </div>
     );
 }
